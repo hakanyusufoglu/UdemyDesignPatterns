989bbad9ae49f71db9a6190e07d84909ff88629b
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2023-09-11 11:49:50 +0300

    Add project files.

---
+++ BaseProject/Program.cs
@@ -0,0 +1,66 @@
+using BaseProject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//MsSql için
+builder.Services.AddDbContext<AppIdentityDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+});
+
+//Identity için
+builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+{
+    options.User.RequireUniqueEmail = true;
+
+}).AddEntityFrameworkStores<AppIdentityDbContext>();
+
+
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    //ServiceProvider ile birlikte yukarýda tanýmladýðýmýz servislere ulaþabilmemizi saðlýyor (GetService servisi alýr ve null olabilir, Ancak GetRequiredService ise bu servisi mutlaka almam lazým diyorum)
+    var identityDbContext = scope.ServiceProvider.GetRequiredService<AppIdentityDbContext>();
+
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
+
+    //Migration yapýlma iþlemi - Uygulama ayaða kalktýðýnda migrationi update-database gibi uygular ve db yoksa otomatik olarak dbyi oluþturur.
+    identityDbContext.Database.Migrate();
+
+    if (!userManager.Users.Any())
+    {
+        userManager.CreateAsync(new AppUser { UserName = "user1", Email = "user1@outlook.com" }, "Password12*").Wait();
+        userManager.CreateAsync(new AppUser { UserName = "user2", Email = "user2@outlook.com" }, "Password12*").Wait(); 
+        userManager.CreateAsync(new AppUser { UserName = "user3", Email = "user3@outlook.com" }, "Password12*").Wait(); 
+        userManager.CreateAsync(new AppUser { UserName = "user4", Email = "user4@outlook.com" }, "Password12*").Wait(); 
+        userManager.CreateAsync(new AppUser { UserName = "user5", Email = "user5@outlook.com" }, "Password12*").Wait(); 
+    }
+}
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
