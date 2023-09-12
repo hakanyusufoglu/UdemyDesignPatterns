04cc9be8e93c2e9379f0ae6934e40849d538a033
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2023-09-12 15:51:54 +0300

    MongoDb için repository gerçekleştirildi.

---
+++ WebApp.Strategy/Repositories/ProductRepositoryFromMongoDb.cs
@@ -0,0 +1,53 @@
+﻿using MongoDB.Driver;
+using WebApp.Strategy.Models;
+
+namespace WebApp.Strategy.Repositories
+{
+    public class ProductRepositoryFromMongoDb : IProductRepository
+    {
+        private readonly IMongoCollection<Product> _productCollection;
+
+        public ProductRepositoryFromMongoDb(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("MongoDb");
+
+            //MongoDb'ye bağlantı için
+            var client = new MongoClient(connectionString);
+
+            //Db yoksa baştan oluşşturacaktır
+            var database = client.GetDatabase("ProductDb");
+
+            //Tablo ismi Products olsun
+            _productCollection = database.GetCollection<Product>("Products");
+
+        }
+
+        public async Task DeleteAsync(Product product)
+        {
+            await _productCollection.DeleteOneAsync(x=>x.Id == product.Id);
+        }
+
+        public async Task<List<Product>> GetAllByUserIdAsync(string userId)
+        {
+            return await _productCollection.Find(x => x.UserId == userId).ToListAsync();
+        }
+
+        public async Task<Product> GetByIdAsync(string id)
+        {
+            return await _productCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<Product> SaveAsync(Product product)
+        {
+            await _productCollection.InsertOneAsync(product);
+
+            //Bu producta yeni eklenen product'ın Id'side var
+            return product; 
+        }
+
+        public async Task UpdateAsync(Product product)
+        {
+            await _productCollection.FindOneAndReplaceAsync(x => x.Id == product.Id, product);
+        }
+    }
+}
