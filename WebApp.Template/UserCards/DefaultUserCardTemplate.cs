396c24e3dfe4f2a617af08ff8c9562f90b321e5d
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2023-09-22 13:30:04 +0300

    UserCardTemplate soyut sınıfı oluşturuldu. DefaultUserCardTemplate sınıfı oluşturularak kullanıcı girişi yapmayan kullanıcının kartının altyapısı hazırlandı

---
+++ WebApp.Template/UserCards/DefaultUserCardTemplate.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApp.Template.UserCards
+{
+    public class DefaultUserCardTemplate : UserCardTemplate
+    {
+        protected override string SetFooter()
+        {
+            return string.Empty;
+        }
+
+        protected override string SetPicture()
+        {
+            //Dinamik olarak kayıt olmayan kullanıcı ikonunu gösteren kısımdır.
+            return $"<img class='card-img-top' stc='/userpictures/defaultuserpicture.png'";
+        }
+    }
+}
---
+++ WebApp.Template/UserCards/UserCardTemplate.cs
@@ -0,0 +1,38 @@
+﻿using BaseProject.Models;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+
+namespace WebApp.Template.UserCards
+{
+    //Nasıl Data geleceğini bilmiyor, soyutluyoruz. Algoritmanın kemiği, yapısını burası belirliyor. Resim alanı yuvarlak mı olacak vs hepsini burası belirliyor.
+    public abstract class UserCardTemplate
+    {
+        protected AppUser AppUser { get; set; }
+        public void SetUser(AppUser appUser)
+        {
+            AppUser = appUser;
+        }
+
+        //Algoritmanın tüm sırası burada belirlenir.
+        public string Build()
+        {
+            if (AppUser == null) throw new ArgumentNullException(nameof(AppUser));
+
+            var sb = new StringBuilder();
+            sb.Append("<div class='card'>"); //birinci kart.
+            //Algoritmanın 1. sırası
+            sb.Append(SetPicture());
+            sb.Append($@"<div class='card-body'<h5>{AppUser.UserName}</h5><p>{AppUser.Description}</p>");
+            sb.Append(SetFooter());
+            sb.Append("</div>");
+
+            sb.Append("</div>");
+            return sb.ToString(); //template hazırz
+        }
+
+        //Değişiklik gösteren kısımlar, alt sınıflarda kullanılabilsin diye protected tanımladık
+        protected abstract string SetFooter();
+
+        protected abstract string SetPicture();
+    }
+}
