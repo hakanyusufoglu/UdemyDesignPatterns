3adda5f0d97ea8b2f22f481bb6cc4b3579a0ab61
Hakan Yusufoğlu <hakan_00133@hotmail.com>
2023-09-25 19:56:19 +0300

    ICommand ve Concrete işlemleri gerçekleştirildi.

---
+++ WebApp.Command/Commands/CreateExcelTableActionCommand.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Command.Commands
+{
+    //Diagramda ICommand'ın Concreat'ine karşılık gelmektedir.
+    public class CreateExcelTableActionCommand<T> : ITableActionCommand
+    {
+        private readonly ExcelFile<T> _excelFile;
+
+        public CreateExcelTableActionCommand(ExcelFile<T> excelFile)
+        {
+            _excelFile = excelFile;
+        }
+
+        public IActionResult Execute()
+        {
+            var excelMemorySteam = _excelFile.Create();
+
+            return new FileContentResult(excelMemorySteam.ToArray(), _excelFile.FileType) { FileDownloadName=_excelFile.FileName};
+        }
+    }
+}
---
+++ WebApp.Command/Commands/ITableActionCommand.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Command.Commands
+{
+    //Diagramda ICommand'a karşılık gelmektedir.
+    public interface ITableActionCommand
+    {
+        IActionResult Execute();
+    }
+}
