97df7d32bd1585410b1cb9f7783d25f4beb86cdb
Hakan Yusufoğlu <hakan_00133@hotmail.com>
2023-09-25 20:01:51 +0300

    FileCreateInvoker sınıfı oluşturuldu

---
+++ WebApp.Command/Commands/FileCreateInvoker.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Command.Commands
+{
+    //Diagram'da invoker'a karşılık gelmektedir.
+    public class FileCreateInvoker
+    {
+        private ITableActionCommand _tableActionCommand;
+        private List<ITableActionCommand> tableActionCommands = new List<ITableActionCommand>();
+
+        public void SetCommand(ITableActionCommand tableActionCommand)
+        {
+            _tableActionCommand = tableActionCommand;
+        }
+        public void AddCommand(ITableActionCommand tableActionCommand)
+        {
+            tableActionCommands.Add(tableActionCommand);
+        }
+
+        public IActionResult CreateFile()
+        {
+            return _tableActionCommand.Execute();
+        }
+
+        public List<IActionResult> CreateFiles()
+        {
+            var list = new List<IActionResult>();
+
+            return tableActionCommands.Select(x => x.Execute()).ToList();
+        }
+    }
+}
