454bd7b51c80f00c4f79237a69f32a7b11c9fec9
Hakan Yusufoğlu <hakan_00133@hotmail.com>
2023-09-26 19:09:42 +0300

    IUserObserver interface'i olusturuldu. Konsol'a user id'yi yazdıracak olan observer kalıtımı alan sınıf oluşturuldu

---
+++ WebApp.Observer/Observer/UserObserverWriteToConsole.cs
@@ -0,0 +1,23 @@
+﻿using BaseProject.Models;
+
+namespace WebApp.Observer.Observer
+{
+    public class UserObserverWriteToConsole : IUserObserver
+    {
+        //DI Container'dan istediğimiz servisi alacağız çünkü ben program.cs'de IUserObserver gördüğünde şu sınıfı türet demiyeceğim
+        
+        //Artık istediğim sınıfı kullanabilirim.
+        private readonly IServiceProvider _serviceProvider;
+
+        public UserObserverWriteToConsole(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public void CreateUser(AppUser appUser)
+        {
+            var logger = _serviceProvider.GetRequiredService<ILogger<UserObserverWriteToConsole>>();
+            logger.LogInformation($"user created : Id={appUser.Id}");
+        }
+    }
+}
