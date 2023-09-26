785daefcbd14c6ac913532eb8e8091a821d9f1ed
Hakan Yusufoğlu <hakan_00133@hotmail.com>
2023-09-26 19:24:20 +0300

    Mail gonderecek UserObserverSendEmail sınıfı olusturuldu

---
+++ WebApp.Observer/Observer/UserObserverSendEmail.cs
@@ -0,0 +1,22 @@
+﻿using BaseProject.Models;
+using System.Net;
+using System.Net.Mail;
+
+namespace WebApp.Observer.Observer
+{
+    public class UserObserverSendEmail : IUserObserver
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public UserObserverSendEmail(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public void CreateUser(AppUser appUser)
+        {
+            //ToDo: Burada kesinlikle mail bilgilerinizi girmeniz bekleniyor.
+     
+        }
+    }
+}
