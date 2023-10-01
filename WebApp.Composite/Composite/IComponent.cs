5c97dda1535009d40058b579a117d464769edf6f
Hakan Yusufoğlu <hakan_00133@hotmail.com>
2023-10-01 14:00:26 +0300

    IComponent ve BookComponent oluşturuldu

---
+++ WebApp.Composite/Composite/IComponent.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApp.Composite.Composite
+{
+    public interface IComponent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        //Bu componenti implemente eden değer hep artacak
+        int Count();
+        string Display();
+    }
+}
