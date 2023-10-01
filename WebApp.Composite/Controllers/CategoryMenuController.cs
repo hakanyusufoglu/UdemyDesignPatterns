26d6ef222622cf2fb2252695285ec5f8f0ea4f94
Hakan Yusufoğlu <hakan_00133@hotmail.com>
2023-10-01 14:31:19 +0300

    Menu icin recursive metot olusturuldu

---
+++ WebApp.Composite/Controllers/CategoryMenuController.cs
@@ -0,0 +1,59 @@
+﻿using BaseProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using WebApp.Composite.Composite;
+using WebApp.Composite.Models;
+
+namespace WebApp.Composite.Controllers
+{
+    public class CategoryMenuController : Controller
+    {
+        private readonly AppIdentityDbContext _context;
+        public CategoryMenuController(AppIdentityDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //category => bookcomposite karşılık gelir
+            //book => bookcomponent'e karşılık gelir
+
+            //bu yapı sonsuz bir yapı olduğu için recursive metot kullanılır.
+
+            var userId = User.Claims.First(x => x.Type == ClaimTypes.NameIdentifier).Value;
+
+            var categories = await _context.Categories.Include(x => x.Books).Where(x => x.UserId == userId).OrderBy(x => x.Id).ToListAsync();
+
+            var menu = GetMenus(categories, new Category { Name = "TopCategory", Id = 0 }, new BookComposite(0, "TopMenu"));
+            return View();
+        }
+
+        public BookComposite GetMenus(List<Category> categories, Category topCategory, BookComposite topBookComposite, BookComposite last = null)
+        {
+            categories.Where(x => x.ReferenceId == topCategory.Id).ToList().ForEach(categoryItem =>
+            {
+                var bookComposite = new BookComposite(categoryItem.Id, categoryItem.Name);
+
+                //categoryitemin içinde kitaplar olabilir
+                categoryItem.Books.ToList().ForEach(bookItem =>
+                {
+                    bookComposite.Add(new BookComponent(bookItem.Id, bookItem.Name));
+                });
+                if (last != null)
+                {
+                    //Ağaca yeni bir dal ekliyoruz.
+                    last.Add(bookComposite);
+                }
+                else
+                {
+                    topBookComposite.Add(bookComposite);
+                }
+
+                GetMenus(categories, categoryItem, topBookComposite, bookComposite);
+            });
+            return topBookComposite;
+        }
+    }
+}
